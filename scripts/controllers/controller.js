c31baa52bef355d15c7d8bf837d042cb5a4c3c4d
Binh Quan <binhqd@gmail.com>
2015-04-02 15:39:49 +0700

    Done

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -1,4 +1,21 @@
 var myapp = angular.module('myapp', []);
 
-myapp.controller('DataBindingController', function ($scope) {
+myapp.controller('DataBindingController', function($scope, $timeout) {
+	$scope.email = "binhqd@greenglobal.com";
+	$scope.number = 15;
+	$scope.checkme = true;
+
+	$timeout(function() {
+		$scope.$watch("number", function() {
+			var pattern =/^[\d]+$/;
+			if (!pattern.test($scope.number)) {
+				console.log($scope.number);
+				alert('Invalid number');
+				$scope.number = 0;
+			} else {
+				console.log($scope.number);
+			}
+		});
+	}, 500);
+
 });
